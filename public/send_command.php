aaeb0d230ee3c249d75f336533f9537a0d0074d4
berryv97 <vincentberry@outlook.fr>
2024-01-17 18:28:48 +0100

    changement de name et ajout !empty pour evité les vide dans la db

--- public/send_command.php
+++ public/send_command.php
@@ -7,9 +7,9 @@ if (!empty($_GET["command"]) && !empty($_GET["session_vmix"])) {
     // Construire la commande en brut
     $session_vmix = $_GET["session_vmix"];
     $command = $_GET["command"];
-    $input =  $_GET["input"];
-    $value = $_GET["value"];
-    $duration = $_GET["duration"];
+    $input = !empty($_GET["input"]) ? $_GET["input"] : 0;
+    $value = !empty($_GET["value"]) ? $_GET["value"] : 0;
+    $duration = !empty($_GET["duration"]) ? $_GET["duration"] : 0;
 
     $req = db_insert::new_command($session_vmix, $command, $input, $value, $duration);
     echo json_encode(array("Valid" => "Command $req bien envoyée ! Elle va bientôt être exécutée !"));
