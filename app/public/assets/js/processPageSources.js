b093e6f30819ef3886b5f011eea4cbbf18e732c6
berry <berry@LAPTOP-EAO0QV26>
2024-02-28 13:59:31 +1200

    Add item.type

--- app/public/assets/js/processPageSources.js
+++ app/public/assets/js/processPageSources.js
@@ -152,7 +152,7 @@ function processPageSources_updateText(textItems) {
     const htmlTextItems = textItems.map((item, index) => `
         <li>
             <h3>${item.name}</h3>
-            <textarea id="gt_text_${item.index}" type="text" value="${item.value}" onblur="ApiVmixSend('Set${type}','${inputSelect}', this.value + '&SelectedIndex=${index + 1}')">${item.value}</textarea>
+            <textarea id="gt_text_${item.index}" type="text" value="${item.value}" onblur="ApiVmixSend('Set${item.type}','${inputSelect}', this.value + '&SelectedIndex=${index + 1}')">${item.value}</textarea>
         </li>
     `);
 
