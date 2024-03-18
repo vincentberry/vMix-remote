91f9bfd3a1205aad6cd25101657ee45789426e80
vincentberry <berryv@outlook.fr>
2024-03-18 15:15:36 +0100

    🐞 fix(TS): null element
    null notificationContainer

--- app/src/assets/TypeScript/notification.ts
+++ app/src/assets/TypeScript/notification.ts
@@ -1,27 +1,33 @@
 function createNotification(type: string, title: string, description: string) {
     const notificationContainer = document.getElementById('notification-container');
-    const notification = document.createElement('div');
-    notification.className = `notification ${type} in`;
+    if (notificationContainer !== null) {
+        const notification = document.createElement('div');
+        notification.className = `notification ${type} in`;
 
-    const closeButton = document.createElement('span');
-    closeButton.className = 'close-btn';
-    closeButton.innerHTML = '&times;';
-    closeButton.addEventListener('click', () => {
-        notification.classList.remove('in');
+        const closeButton = document.createElement('span');
+        closeButton.className = 'close-btn';
+        closeButton.innerHTML = '&times;';
+        closeButton.addEventListener('click', () => {
+            notification.classList.remove('in');
+            setTimeout(() => {
+                notification.remove();
+            }, 500);
+        });
+
+        notification.innerHTML = `<strong>${title}</strong><br>${description}`;
+        notification.appendChild(closeButton);
+
+
+        notificationContainer.appendChild(notification);
+
         setTimeout(() => {
-            notification.remove();
-        }, 500);
-    });
-
-    notification.innerHTML = `<strong>${title}</strong><br>${description}`;
-    notification.appendChild(closeButton);
-
-    notificationContainer.appendChild(notification);
-
-    setTimeout(() => {
-        notification.classList.remove('in');
-        setTimeout(() => {
-            notification.remove();
-        }, 500);
-    }, 3000); // 5 seconds timeout
+            notification.classList.remove('in');
+            setTimeout(() => {
+                notification.remove();
+            }, 500);
+        }, 3000); // 5 seconds timeout
+    } else {
+        console.error("Notification container element not found.");
+        // Handle the case where notificationContainer is null, e.g., display an error message.
+    }
 }
